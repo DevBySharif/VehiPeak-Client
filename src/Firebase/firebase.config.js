1459e39a3d111eb3868becb76fba815716e70d72
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-10-20 04:05:48 +0600

    configure firebase env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCNd2gl7rt6En9ZOr3DhQXPqDjAnZIRBxI",
-  authDomain: "vehipeak.firebaseapp.com",
-  projectId: "vehipeak",
-  storageBucket: "vehipeak.appspot.com",
-  messagingSenderId: "576961502416",
-  appId: "1:576961502416:web:6d288e7c109f11edbd52d5"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
